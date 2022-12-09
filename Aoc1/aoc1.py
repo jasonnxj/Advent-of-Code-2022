58a78e385c35fe43e54aa5f5ad31af28992e5c75
Jason Ng <56112444+jasonnxj@users.noreply.github.com>
2022-12-09 12:52:42 +0800

    first commit

---
+++ Aoc1/aoc1.py
@@ -0,0 +1,13 @@
+biggest = 0
+current = 0
+
+with open('aoc1-input.txt') as f:
+    calories = f.readlines()
+    for cal in calories:
+        cal = cal.strip()
+        if cal != "":
+            current += int(cal)
+        else:
+            biggest = max(biggest, current)
+            current = 0
+print(biggest)
---
+++ Aoc1/aoc1a.py
@@ -0,0 +1,15 @@
+top_three = [0,0,0]
+current = 0
+
+with open('aoc1-input.txt') as f:
+    calories = f.readlines()
+    for cal in calories:
+        cal = cal.strip()
+        if cal != "":
+            current += int(cal)
+        else:
+            top_three.sort()
+            top_three[0] = max(top_three[0], current)
+            current = 0
+print(top_three)
+print(sum(top_three))
